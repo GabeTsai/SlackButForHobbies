128c6ec7924171a1513e99334af30d872e66e9db
Gabriel Tsai <gabrielhtsai@gmail.com>
2025-01-06 01:17:46 -0800

    change getStaticProps to generateStaticParams

--- app/channels/[id]/page.tsx
+++ app/channels/[id]/page.tsx
@@ -2,15 +2,22 @@ import pb from "../../lib/pocketbase";
 import ChannelPage from "./channelPage";
 // import { UserProvider } from "../../context/userContext";
 
-export default async function getStaticProps({params}: {params: {id: string}}) {
+export default async function Page({ params }: { params: { id: string } }) {
     const channel = await pb.collection("channels").getOne(params.id);
 
     return (
-        // <UserProvider>
-            <div>
-            <h1> {channel.name} </h1>
-            <ChannelPage channel={channel}/>
-            </div>
-        // </UserProvider>
+        <div>
+            <h1>{channel.name}</h1>
+            <ChannelPage channel={channel} />
+        </div>
     );
+}
+
+// prerender corresponding pages for each id during build process
+export async function generateStaticParams() {
+    const channels = await pb.collection("channels").getFullList();
+
+    return channels.map((channel: { id: string }) => ({
+        id: channel.id,
+    }));
 }
